63c03f0c377715c96429b2357f964b02072680ef
dnwu <443881762@qq.com>
2018-08-06 17:01:24 +0800

    系统操作完成

--- src/pages/main/System/System.vue
+++ src/pages/main/System/System.vue
@@ -6,30 +6,148 @@
                 <el-breadcrumb-item>系统操作</el-breadcrumb-item>
             </el-breadcrumb>
         </div>
+        <div class="contain">
+            <div class="title">修改密码</div>
+            <div class="reset-password">
+                <el-form size="mini" :model="repasswordData" ref="repasswordData" label-width="100px" class="demo-ruleForm">
+                    <el-form-item
+                        label="用户名"
+                        prop="username"
+                        :rules="[
+                        { required: true, message: '用户名不能为空'}
+                        ]"
+                    >
+                        <el-input v-model="repasswordData.username" auto-complete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item
+                        label="密码"
+                        prop="pass"
+                        :rules="[
+                        { required: true, message: '密码不能为空'}
+                        ]"
+                    >
+                        <el-input v-model="repasswordData.pass" auto-complete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item
+                        label="新密码"
+                        prop="newPass"
+                        :rules="[
+                        { required: true, message: '新密码不能为空'}
+                        ]"
+                    >
+                        <el-input v-model="repasswordData.newPass" auto-complete="off"></el-input>
+                    </el-form-item>
+                    <el-form-item>
+                        <el-button type="primary" @click="submitForm('repasswordData')">修改密码</el-button>
+                    </el-form-item>
+                </el-form>
+            </div>
+            <div class="title">恢复出厂设置</div>
+            <div class="reboot">
+                <div class="reboot-dialog">
+                    <el-dialog
+                        :visible.sync="rebootBox"
+                        width="320px">
+                        <div class="body">
+                            <div class="icon el-icon-warning"></div>
+                            <p>该操作将会重置数据，是否确认执行该操作</p>
+                            <p></p>
+                        </div>
+                        <span slot="footer" class="dialog-footer">
+                            <el-button plain type='primary' size="mini" @click="rebootBox = false">取消</el-button>
+                            <el-button plain type='danger' size="mini" @click="rebootBox = false">确定</el-button>
+                        </span>
+                    </el-dialog>
+                </div>
+                <p>点击按钮，将会清空项目数据，不用重新激活</p>
+                <el-button plain type="primary" @click="rebootBtn">恢复出厂设置</el-button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        data() {
-            return {
-                key: "value"
-            }
-        },
+export default {
+  data() {
+    return {
+      rebootBox: false,
+      repasswordData: {
+        username: "",
+        pass: "",
+        newPass: ""
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          alert("submit!");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    rebootBtn() {
+        this.rebootBox = true
     }
+  }
+};
 </script>
 
 <style lang='less' scoped>
-.system{
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-    .bread-crumb{
-        padding-left: 20px;
-        height: 40px;
-        background-color: #FAFAFA;
-        display: flex;
-        align-items: center;
+.system {
+  width: 100%;
+  height: 100%;
+  background-color: #fff;
+  .bread-crumb {
+    padding-left: 20px;
+    height: 40px;
+    background-color: #fafafa;
+    display: flex;
+    align-items: center;
+  }
+  .contain {
+    .title {
+      height: 40px;
+      background-color: #fafafa;
+      line-height: 40px;
+      border-left: 6px solid #1890ff;
+      padding-left: 10px;
+      font-size: 14px;
+      font-weight: 700;
     }
+    .reset-password {
+      width: 500px;
+      margin: 20px auto;
+      padding: 0 100px;
+      box-sizing: border-box;
+    }
+    .reboot {
+      padding-top: 20px;
+      text-align: center;
+      color: #333333;
+      .el-button {
+        margin-top: 20px;
+      }
+    }
+  }
+}
+</style>
+<style lang="less">
+.reboot-dialog {
+  .el-dialog__body {
+    .body {
+      .icon {
+        font-size: 40px;
+        color: #f5222d;
+      }
+      p:nth-of-type(1) {
+        font-size: 14px;
+        font-weight: 700;
+      }
+    }
+  }
 }
 </style>
